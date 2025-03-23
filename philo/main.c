9f3e61c134c0cb7f262fb25e214d69bf5c3ec180
imiqor <iqorimane111@gmail.com>
2025-03-23 07:06:23 +0000

    init

---
+++ philo/main.c
@@ -0,0 +1,16 @@
+#include"header.h"
+
+int main(int argc,char** argv)
+{
+    // t_philo philo;
+    t_data data;
+    (void) argc;
+    data.philo_num = ft_atoi(argv[1]);
+    // data.i = 0;
+    // while(data.i < data.philo_num)
+    // {
+
+    // }
+
+    printf("%i\n",data.philo_num);
+}
